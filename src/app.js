053de6ccd7d288be9e2476e4b49c5844ab7f307e
Piyush-2704 <piyushiiitp@gmail.com>
2025-07-21 22:48:32 +0530

    start of route configuration and controller creation

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import cookieOarser from "cookie-parser";
+import cookieParser from "cookie-parser";
 
 const app=express();
 
@@ -13,5 +13,15 @@ app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended: true,limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+
+//Routes import
+
+import userRouter from './routes/user.routes.js';
+
+
+//Routes declaration
+app.use("/api/v1/users",userRouter)
+
 
 export { app }
